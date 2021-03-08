c6fb2cec4744ea5d4dd19f643b9d96233af31b8d
Tanasit Auewongaree <006006@cdg.co.th>
2021-03-08 16:23:37 +0700

    Initial Commit

---
+++ SUI01/SUI01App.swift
@@ -0,0 +1,17 @@
+//
+//  SUI01App.swift
+//  SUI01
+//
+//  Created by BillY-MAC on 8/3/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SUI01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
